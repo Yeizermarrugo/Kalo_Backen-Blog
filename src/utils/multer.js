164f2f98f713d2ab1cdc058f2049f7ec280071d1
Yeizer Marrugo <yeizermarrugo@gmail.com>
2023-05-25 16:30:04 -0500

    [MOD] img multer

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.resolve('uploads'));
+    cb(null, path.resolve('../../uploads'));
   },
   filename: (req, file, cb) => {
     const timestamp = Math.floor(Date.now() / 1000); // Obtener el tiempo en segundos
